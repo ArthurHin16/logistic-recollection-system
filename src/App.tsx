74f8b149ef4f124a280aea6d7c9ba3b6354bf981
Efren73 <A01276266@itesm.mx>
2021-09-27 23:38:42 -0500

    Primer commit feature Efren

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { LoginRoute } from './pages/login.route';
+import { CoordinatorRoute } from './pages/coordinator.route';
 
 export const App: FC = (): JSX.Element => {
   return (
     <BrowserRouter>
         <Switch>
           <Route path='/' exact component = { LoginRoute }/>
+          <Route path='/coordinator' component = {CoordinatorRoute}/>
         </Switch>
     </BrowserRouter>
   );
